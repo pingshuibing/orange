2688706dc21e40b6612dfd21cfd88d9c24a6db02
simensma <simensma@gmail.com>
2012-08-24 15:49:18 +1000

    Added dummy Panel XML to the front page

--- SolarPowerCalc/src/com/qut/spc/SolarPowerCalcServlet.java
+++ SolarPowerCalc/src/com/qut/spc/SolarPowerCalcServlet.java
@@ -2,10 +2,13 @@ package com.qut.spc;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+
 @SuppressWarnings("serial")
 public class SolarPowerCalcServlet extends HttpServlet {
+	String text="<panels>"+"<panel>"+"<id>1</id>"+"<name>A Panel</name>"+"<model>A model</model>"+"<manufacturer>ACompany</manufacturer>"+"<price>1233.12</price>"+"<companyWebsite>www.somewebsite.com</companyWebsite>"+"<capacity>someCapcity</capacity>"+"</panel>"+"<panel>"+"<id>1</id>"+"<name>A Panel</name>"+"<model>A model</model>"+"<manufacturer>ACompany</manufacturer>"+"<price>1233.12</price>"+"<companyWebsite>www.somewebsite.com</companyWebsite>"+"<capacity>someCapacity</capacity>"+"</panel>"+"</panels>";
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("application/xml");
+		
+		resp.getWriter().println(text);
 	}
 }
