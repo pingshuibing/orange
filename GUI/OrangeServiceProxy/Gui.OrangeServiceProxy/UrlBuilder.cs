66fbe882c0fe039ae0d33250b3598f0c661d97a8
alan soto <me@alansoto.info>
2012-08-29 09:03:26 +1000

    rearrrange directories and files into one VS studio
    we now have one big visual studio solution containing 4 projects:
    - SeleniumTests
    - WebApplication
    - OrangeServiceProxy  (this one contains a custom url builder to server
    side make calls to the google apps engine and bypass crossdomain issues
    with javascript)
    - OrangeServiceProxy_Tests (this one contains all NUnit tests for the
    OrangeServiceProxy project)

---
+++ GUI/OrangeServiceProxy/Gui.OrangeServiceProxy/UrlBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUI.OrangeServiceProxy
+{
+    public class UrlBuilder
+    {
+        public string OperationName { get; set; }
+        public string MyProperty { get; set; }
+
+        public int addnumbers(int a, int b)
+        {
+            return 34;
+        }
+    }
+}
