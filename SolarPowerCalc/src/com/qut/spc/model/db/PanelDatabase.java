4c2838d8a300ec5c2baa3ead5226a88726d6514b
simensma <simensma@gmail.com>
2012-08-30 23:59:09 +1000

    Deleted converters

--- SolarPowerCalc/src/com/qut/spc/model/db/PanelDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.qut.spc.model.db;
-
-import java.util.List;
-
-import com.qut.spc.model.Panel;
-
-public interface PanelDatabase {
-	public List<Panel> getPanelsInPriceRange(double min, double max);
-}
