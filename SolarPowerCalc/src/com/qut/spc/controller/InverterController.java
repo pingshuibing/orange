5c8fe7bede5e910aa5c14f0fc38b85b2938531ed
Quoc-Viet Nguyen <afelion@gmail.com>
2012-09-03 17:57:47 +1000

    Add inverter and battery controller

---
+++ SolarPowerCalc/src/com/qut/spc/controller/InverterController.java
@@ -0,0 +1,44 @@
+package com.qut.spc.controller;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.qut.spc.api.InverterFilterAPI;
+import com.qut.spc.exceptions.InvalidArgumentException;
+import com.qut.spc.model.InverterContainer;
+
+@Path("/inverter/")
+public class InverterController {
+	private InverterFilterAPI db;
+	
+	public InverterController() {
+		db = new InverterContainer();
+	}
+	
+	@GET
+	@Produces("application/xml")
+	@Path("/")
+	public InverterFilterAPI getInvertersByPriceCapacityLocation(
+			@QueryParam("priceMin") @DefaultValue("0") double minPrice,
+			@QueryParam("priceMax") @DefaultValue("0") double maxPrice,
+			@QueryParam("capacityMin") @DefaultValue("0") double minCapacity,
+			@QueryParam("capacityMax") @DefaultValue("0") double maxCapacity,
+			@QueryParam("postcode") @DefaultValue("0000") String postcode)
+					throws InvalidArgumentException{
+		try {
+			db.setMaxCapacity(maxCapacity);
+			db.setMaxPrice(maxPrice);
+			db.setMinPrice(minPrice);
+			db.setMinCapacity(minCapacity);
+			db.setPostcode(postcode);
+			db.search();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return db;
+	}
+}
