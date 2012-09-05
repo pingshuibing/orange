2b2d9456298be38f7f30944234dd022335c8cbce
MajidS <Majidbox@gmail.com>
2012-09-05 11:34:58 +1000

    Connecting to service and asking for a price range

--- SolarPowerCalc_Android/src/com/qut/spc/MainActivity.java
+++ SolarPowerCalc_Android/src/com/qut/spc/MainActivity.java
@@ -1,21 +1,90 @@
 package com.qut.spc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.qut.spc.R.id;
+
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
 
+	EditText MinPric,MaxPrice,OutText;
+	Button Button_BetPrice;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button_BetPrice=(Button) findViewById(R.id.GetPricesButton);
+       MinPric=(EditText)findViewById(R.id.TextMinPrice);
+       MaxPrice=(EditText)findViewById(R.id.TextMaxPrice);
+       OutText=(EditText) findViewById(R.id.outText);
+       Button_BetPrice.setOnClickListener(new View.OnClickListener() {
+   	    public void onClick(View v) {
+   	        // Do something in response to button click
+   	    	OutText.setText(GetPanelPrices(MinPric.getText().toString(),MaxPrice.getText().toString()));
+   	    }
+   	});
         
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    
+    private String GetPanelPrices(String priceMin,String priceMax)
+    {
+    	
+    	 return JSONGETRequest("http://solarpowercalc.appspot.com/component?priceMin="+priceMin+"&priceMax="+priceMax);
     }
+    
+   private String JSONGETRequest(String ServiceRequest)
+   {
+	   String Output="error";
+	   try {
+		   
+			URL url = new URL(ServiceRequest);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+	 
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+	 
+			String output;
+			Output="Output from Server .... \n";
+			while ((output = br.readLine()) != null) {
+				Output+=output;
+			}
+	 
+			conn.disconnect();
+	 
+		  } catch (MalformedURLException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (IOException e) {
+	 
+			e.printStackTrace();
+	 
+		  }
+	   return Output;
+   }
+
+	public void onSuccess() {
+		// TODO Auto-generated method stub
+		
+	}
 }
