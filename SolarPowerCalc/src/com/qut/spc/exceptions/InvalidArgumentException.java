3b9e30409e8ecfe147a9243cb1dee73c93a20a70
Quoc-Viet Nguyen <afelion@gmail.com>
2012-09-10 04:31:50 +1000

    Change HTTP return code to 400 (Bad request)

--- SolarPowerCalc/src/com/qut/spc/exceptions/InvalidArgumentException.java
+++ SolarPowerCalc/src/com/qut/spc/exceptions/InvalidArgumentException.java
@@ -5,13 +5,16 @@ import javax.ws.rs.core.Response;
 
 import com.sun.jersey.api.Responses;
 
-public class InvalidArgumentException extends WebApplicationException{
-	public InvalidArgumentException(){
+public class InvalidArgumentException extends WebApplicationException {
+
+	private static final long serialVersionUID = 4617727113561585926L;
+
+	public InvalidArgumentException() {
 		super(Responses.notAcceptable().build());
 	}
-	
-	public InvalidArgumentException(String message){
-		super(Response.status(500).entity(message).type("text/plain").build());
+
+	public InvalidArgumentException(String message) {
+		super(Response.status(400).entity(message).type("text/plain").build());
 	}
 
 }
