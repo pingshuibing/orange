34eeb90ed2abdc4c512fded5697b319b9e5724c3
Quoc-Viet Nguyen <afelion@gmail.com>
2012-09-13 18:13:31 +1000

    Try to save activity state

--- SolarPowerCalc_Android/src/com/qut/spc/MainActivity.java
+++ SolarPowerCalc_Android/src/com/qut/spc/MainActivity.java
@@ -1,5 +1,7 @@
 package com.qut.spc;
 
+import java.util.Arrays;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,37 +14,92 @@ import android.widget.Spinner;
 public class MainActivity extends Activity {
 	final String SPC_URL = "http://solarpowercalc.appspot.com";
 
+	final String[] componentTypes = {
+			"Panels",
+			"Iinverters",
+			"Batteries",
+	};
+
 	private Spinner spComponent;
 	private EditText etPostcode, etPriceMin, etPriceMax, etCapacityMin, etCapacityMax;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        spComponent = (Spinner)findViewById(R.id.spComponent);
-        etPostcode = (EditText)findViewById(R.id.postcode);
-        etPriceMin = (EditText)findViewById(R.id.priceMin);
-        etPriceMax = (EditText)findViewById(R.id.priceMax);
-        etCapacityMin = (EditText)findViewById(R.id.capacityMin);
-        etCapacityMax = (EditText)findViewById(R.id.capacityMax);
-        
-        String[] list = new String[] {
-        		"Panels",
-        		"Iinverters",
-        		"Batteries",
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-        		android.R.layout.simple_spinner_dropdown_item, list);
-        spComponent.setAdapter(adapter);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		spComponent = (Spinner)findViewById(R.id.spComponent);
+		etPostcode = (EditText)findViewById(R.id.postcode);
+		etPriceMin = (EditText)findViewById(R.id.priceMin);
+		etPriceMax = (EditText)findViewById(R.id.priceMax);
+		etCapacityMin = (EditText)findViewById(R.id.capacityMin);
+		etCapacityMax = (EditText)findViewById(R.id.capacityMax);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_dropdown_item, componentTypes);
+		spComponent.setAdapter(adapter);
+
+		restoreInstanceState(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+
+		state.putString("type", spComponent.getSelectedItem().toString());
+		state.putString("postcode", etPostcode.getText().toString());
+		state.putString("priceMin", etPriceMin.getText().toString());
+		state.putString("priceMax", etPriceMax.getText().toString());
+		state.putString("capacityMin", etCapacityMin.getText().toString());
+		state.putString("capacityMax", etCapacityMax.getText().toString());
+	}
+	
+	@Override
+	// onRestoreInstanceState is only called when the program is terminated
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+
+		restoreInstanceState(savedInstanceState);
+	}
+
+	private void restoreInstanceState(Bundle state) {
+		if (state == null) {
+			return;
+		}
+		String str;
+		str = state.getString("type");
+		if (str.length() > 0) {
+			int pos = Arrays.asList(componentTypes).indexOf(str);
+			spComponent.setSelection(pos);
+		}
+		str = state.getString("postcode");
+		if (str.length() > 0) {
+			etPostcode.setText(str);
+		}
+		str = state.getString("priceMin");
+		if (str.length() > 0) {
+			etPriceMin.setText(str);
+		}
+		str = state.getString("priceMax");
+		if (str.length() > 0) {
+			etPriceMax.setText(str);
+		}
+		str = state.getString("capacityMin");
+		if (str.length() > 0) {
+			etCapacityMin.setText(str);
+		}
+		str = state.getString("capacityMax");
+		if (str.length() > 0) {
+			etCapacityMax.setText(str);
+		}
+	}
+
     public void onSearchClick(View v) {
     	int postcode = 0;
     	int priceMin = 0;
