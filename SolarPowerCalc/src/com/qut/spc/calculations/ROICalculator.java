b5ce98bef2917db2564f822f2056b03b62a2cea1
zuojun chen <pingshuibing@gmail.com>
2012-09-13 15:11:34 +1000

    Implement ROICalculator API
    Implement ROICalculator API

---
+++ SolarPowerCalc/src/com/qut/spc/calculations/ROICalculator.java
@@ -0,0 +1,43 @@
+package com.qut.spc.calculations;
+
+import com.qut.spc.api.ROICalculationAPI;
+
+public class ROICalculator implements ROICalculationAPI {
+	
+	private double systemSize, feedInTariff, costOfElectricity, dailyUsage, dailySunHours, systemCost;
+	
+
+	@Override
+	public void setSystemSize(double size) throws IllegalArgumentException {
+		this.systemSize = size;
+	}
+
+	@Override
+	public void setFeedInTariff(double tariff) throws IllegalArgumentException {
+		this.feedInTariff = tariff;
+	}
+
+	@Override
+	public void setCostOfElectricity(double cost)
+			throws IllegalArgumentException {
+		this.costOfElectricity = cost;
+	}
+
+	@Override
+	public void setDailyUsage(double usage) throws IllegalArgumentException {
+		this.dailyUsage = usage;
+	}
+
+	@Override
+	public void setDailySunHours(double hours) throws IllegalArgumentException {
+		this.dailySunHours = hours;
+	}
+
+	@Override
+	public void setSystemCost(double cost) throws IllegalArgumentException {
+		this.systemCost = cost;
+	}
+	
+	
+
+}
