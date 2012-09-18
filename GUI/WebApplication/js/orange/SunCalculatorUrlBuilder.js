518c31ede1a1d8b754f7d1e1ba44684523c37000
alan soto <me@alansoto.info>
2012-09-18 10:07:39 +1000

    add unit tests and implement UrlBuilder for all new functions on API

--- GUI/WebApplication/js/orange/SunCalculatorUrlBuilder.js
+++ GUI/WebApplication/js/orange/SunCalculatorUrlBuilder.js
@@ -10,34 +10,43 @@ function UrlBuilder()
     this.MinimumCapacity = "";
     this.MaximumCapacity = "";
     this.Postcode = "";
+    this.Operation = "";
+    this.InverterId = "";
+    this.BatteryId = "";
+    this.PanelId = "";
+    this.SystemCost = "";
+    this.InverterEfficiency = "";
+    this.PanelEfficiency = "";
+    this.PanelOutput = "";
+    this.PanelCount = "";
 
-    this.toString = function ()
-    {
+
+    this.toString = function () {
         var completeUrl = "";
 
-        if (this.GoogleAppsEngineBaseUrl != "")
-        {
+        if (this.GoogleAppsEngineBaseUrl != "") {
             var baseUrl = "";
             var parameters = "";
 
             baseUrl += this.GoogleAppsEngineBaseUrl;
 
-            if (this.ComponentName != "")
-            {
+            if (this.ComponentName != "") {
                 baseUrl += "/" + this.ComponentName;
             }
 
-            if (this.MinimumPrice != "" || this.MaximumPrice != "")
-            {
+            if (this.Operation != "") {
+                baseUrl += "/" + this.Operation;
+            }
+
+            //start search parameters
+            if (this.MinimumPrice != "" || this.MaximumPrice != "") {
 
                 var min = 0; var max = 0;
-                if (this.MinimumPrice <= this.MaximumPrice)
-                {
+                if (this.MinimumPrice <= this.MaximumPrice) {
                     min = this.MinimumPrice;
                     max = this.MaximumPrice;
                 }
-                else
-                {
+                else {
                     min = this.MaximumPrice;
                     max = this.MinimumPrice;
                 }
@@ -45,21 +54,17 @@ function UrlBuilder()
                 parameters += "&priceMin=" + min + "&priceMax=" + max;
             }
 
-            if (this.Postcode != "")
-            {
+            if (this.Postcode != "") {
                 parameters += "&postcode=" + this.Postcode;
             }
 
-            if (this.MinimumCapacity != "" || this.MaximumCapacity != "")
-            {
+            if (this.MinimumCapacity != "" || this.MaximumCapacity != "") {
                 var min = 0; var max = 0;
-                if (this.MinimumCapacity <= this.MaximumCapacity)
-                {
+                if (this.MinimumCapacity <= this.MaximumCapacity) {
                     min = this.MinimumCapacity;
                     max = this.MaximumCapacity;
                 }
-                else
-                {
+                else {
                     min = this.MaximumCapacity;
                     max = this.MinimumCapacity;
                 }
@@ -67,8 +72,27 @@ function UrlBuilder()
                 parameters += "&capacityMin=" + min + "&capacityMax=" + max;
             }
 
-            if (parameters.indexOf("&") == 0)
-            {
+            //start calculations of efficiency parameters
+            if (this.PanelId != "" && this.InverterId != "" && this.BatteryId != "") {
+                parameters += "&inverter=" + this.InverterId + "&battery=" + this.BatteryId + "&panel=" + this.PanelId;
+                if (this.PanelCount != "") {
+                    parameters += "&panelCount=" + this.PanelCount;
+                }
+                else {
+                    parameters += "&panelCount=1";
+                }
+            }
+            else {
+                if (this.SystemCost != "") {
+                    parameters += "&systemCost=" + this.SystemCost + "&inverterEfficiency=" + this.InverterEfficiency + "&panelEfficiency=" + this.PanelEfficiency + "&panelOutput=" + this.PanelOutput;
+                }
+            }
+
+
+
+
+            //building complete URL
+            if (parameters.indexOf("&") == 0) {
                 parameters = parameters.replace(parameters.charAt(0), "");
             }
 
