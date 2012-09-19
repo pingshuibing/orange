ca4c7fac2cd07214ad6ad115ecc0f7f20c95ecff
zuojun chen <pingshuibing@gmail.com>
2012-09-20 00:54:38 +1000

    ADD paremeter in ROI & ElectricityProction
    add time span parameter in ROICalculatro,
    add dimesion parameter in ElectricityProction

--- SolarPowerCalc/src/com/qut/spc/api/ElectricityCalculationApi.java
+++ SolarPowerCalc/src/com/qut/spc/api/ElectricityCalculationApi.java
@@ -4,17 +4,18 @@ public interface ElectricityCalculationApi {
 	
 	/**
 	 * Get the total electricity production
-	 * 
-	 * @param dailySun Average  daily sun light (kWh, >0)
+	 * @param dimensions The dimension of a panel(square meter, >0)
+	 * @param dailySun Average  daily sun light per square meter (kW/square meter, >0)
 	 * @param inverterEfficiency The efficiency of a inverter (0-1)
 	 * @param solarPanelEfficiency The efficiency of a solar panel (0-1)
 	 * @param solarPowerOutput The capacity of a solar panel (>0)
 	 * @param dailyHours The average daily hours of sunlight (hour, >0)
-	 * @param timespan The time span that user want to calculate (month) (>0)
+	 * @param timespan The time span that user want to calculate (day) (>0)
 	 * 
 	 * @throws IllegalArgumentException If input is invalid
 	 */
 	public double getElectricityProduction(
+			double dimension,
 			double dailySun,
 			double inverterEfficiency,
 			double solarPanelEfficiency,
--- SolarPowerCalc/test/com/qut/spc/calculations/ROICalculatorTest.java
+++ SolarPowerCalc/test/com/qut/spc/calculations/ROICalculatorTest.java
@@ -36,67 +36,79 @@ public class ROICalculatorTest {
 	 */
 	@Test
 	public void test_ROICalculatorParameters() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_electricity_zero() {
-		ROICalulator = new ROICalculator(0, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(0, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test (expected = IllegalArgumentException.class)
 	public void test_ROICalculatorParameters_electricity_negative() {
-		ROICalulator = new ROICalculator(-5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(-5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_electricity_positive() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_feedInTariff_zero() {
-		ROICalulator = new ROICalculator(5, 0, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0, 0.44, 10, 1000, 365);
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void test_ROICalculatorParameters_feedInTariff_negative() {
-		ROICalulator = new ROICalculator(5, -0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, -0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_feedInTariff_positive() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_costOfElectricity_zero() {
-		ROICalulator = new ROICalculator(5, 0.23, 0, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0, 10, 1000, 365);
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void test_ROICalculatorParameters_costOfElectricity_negative() {
-		ROICalulator = new ROICalculator(5, 0.23, -0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, -0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_costOfElectricity_positive() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_dailyUsage_zero() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 0, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 0, 1000, 365);
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void test_ROICalculatorParameters_dailyUsage_negative() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, -10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, -10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_dailyUsage_positive() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_systemCost_zero() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 0);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 0, 365);
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void test_ROICalculatorParameters_systemCost_negative() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, -1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, -1000, 365);
 	}
 	@Test
 	public void test_ROICalculatorParameters_systemCost_positive() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000);
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
+	}
+	@Test
+	public void test_ROICalculatorParameters_timeSpan_zero() {
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 0);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void test_ROICalculatorParameters_timeSpan_negative() {
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, -365);
+	}
+	@Test
+	public void test_ROICalculatorParameters_timeSpan_positive() {
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 1000, 365);
 	}
 
 	
@@ -105,15 +117,15 @@ public class ROICalculatorTest {
 	 */
 	@Test
 	public void test_SetAnnualElectricityProduction_zero() {
-		ROICal.setAnnualElectricityProduction(0);
+		ROICal.setDailyElectricityProduction(0);
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void test_SetAnnualElectricityProduction_negative() {
-		ROICal.setAnnualElectricityProduction(-100);
+		ROICal.setDailyElectricityProduction(-100);
 	}
 	@Test
 	public void test_SetAnnualElectricityProduction_positive() {
-		ROICal.setAnnualElectricityProduction(100);
+		ROICal.setDailyElectricityProduction(100);
 	}
 
 	/**
@@ -179,14 +191,31 @@ public class ROICalculatorTest {
 	public void test_SetSystemCost_positive() {
 		ROICal.setSystemCost(1000);
 	}
+	
+	/**
+	 * Test method for {@link com.qut.spc.calculations.ROICalculator#setSystemCost(double)}.
+	 */
+	@Test
+	public void test_SetTimeSpan_zero() {
+		ROICal.setTimeSpan(0);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void test_SetTimeSpan_negative() {
+		ROICal.setTimeSpan(-365);
+	}
+	@Test
+	public void test_SetTimeSpan_positive() {
+		ROICal.setTimeSpan(365);
+	}
 
 	/**
 	 * Test method for {@link com.qut.spc.calculations.ROICalculator#getAnnualROI()}.
 	 */
 	@Test
-	public void test_GetAnnualROI() {
-		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 10000);
-		assertEquals(0.12,ROICalulator.getAnnualROI(),0.01);
+	public void test_GetROI() {
+		ROICalulator = new ROICalculator(5, 0.23, 0.44, 10, 10000,365);
+		//System.out.println (ROICalulator.getROI());
+		assertEquals(0.12,ROICalulator.getROI(),0.01);
 	}
 
 }
